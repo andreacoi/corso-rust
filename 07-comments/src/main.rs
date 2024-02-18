d5ac98242fb0fd38b137581d5c8a8d0cd6eaaaeb
Andrea <andrea.coi@outlook.it>
2024-02-18 14:55:21 +0100

    terminato settimo capitolo - commenti
    capitolo senza codice

---
+++ 07-comments/src/main.rs
@@ -0,0 +1,8 @@
+// commenti in in rust
+// I commenti in Rust  si indicano con il doppio slash. Non ci sono altri modi per fare un
+// commento.
+// Esistono i commenti per produrre una documentazione. Si fanno utilizzando i tre slash: ///
+
+fn main() {
+    println!("Hello, world!");
+}

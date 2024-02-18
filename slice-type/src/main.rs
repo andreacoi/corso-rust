dd734d645c17430b8b2d2a8261a6dc883d0f1ce6
Andrea Coi <andrea.coi@outlook.it>
2024-02-18 18:26:45 +0100

    prendo confidenza con gli slice type

--- slice-type/src/main.rs
+++ slice-type/src/main.rs
@@ -21,9 +21,36 @@ fn first_word(s: &String) -> usize {
     // altrimenti ritorna la lunghezza dell'intera stringa
     s.len()
 }
+// questa funzione può essere riscritta utilizzando gli slice in questo modo:
+//
+fn first_word_with_slices(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
 
 fn main() {
-    let s = String::from("Hello World");
-    let i = first_word(&s);
-    println!("{}", i);
+    // rendo mutable la stringa
+    let s = String::from("He Man");
+    let word = first_word(&s);
+
+    let real_word = first_word_with_slices(&s);    // word rimane uguale a 5! Questo non va bene.
+    println!("{}", word);
+    println!("{}", real_word);
+
+    let sliced = slicer(&s);
+    println!("{}", sliced);
+}
+// imposto funzione che ritorna una parte di una stringa attraverso il tipo slice. Il tipo slice si
+// dichiara, come si può vedere in basso come &str.
+// Il tipo slice funziona esattamente come gli array di caratteri per il PHP, uniti costituiscono
+// una stringa, ma possono essere separati tramite l'indicazione dell'indice.
+fn slicer(mia_stringa: &String) -> &str {
+    let sliced = &mia_stringa[0..3]; // è uguale a let sliced = &mia_stringa[..3]; lo stesso vale
+    // per il trailing number finale let sliced = &mia_stringa[0..]
+    sliced
 }

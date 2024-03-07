1b5a53009348a4bf225a939984410d52f6dd41c5
Andrea <andrea.coi@outlook.it>
2024-03-07 23:47:19 +0100

    path assoluti e path relativi
    utilizzo della keyword pub e comprensione del meccanismo nonno > padre > figlio alla base dei path dei moduli

--- capitolo-07/paths-referencing-item-modules/restaurant/src/lib.rs
+++ capitolo-07/paths-referencing-item-modules/restaurant/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Per poter utilizzare i moduli dichiarati nelle librerie ci si può riferire ad essi come quando
+// si esplorano le cartelle di un filesystem.
+// Per questo motivo i path di un modulo si presentano in due forme:
+// - path assoluti: partono dalla root del crate utilizzando la parola chiave crate. Dopo ogni
+// livello, la separazione degli stessi avviene utilizzando i due punti ripetuti due volte: "(::)"
+// vedi r. 21
+// - path relativi: partono dal modulo corrente ed utilizzano keyword come super o self per
+// riferirsi al modulo corrente.
+//
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+mod frontend_ristorante {
+    mod accoglienza {
+        fn accogli_clientela() {}
     }
 }
+
+mod backend_ristorante {}
+
+pub fn mangia_al_ristorante() {
+    // esempio di path assoluto
+    crate::frontend_ristorante::accoglienza::accogli_clientela();
+    // esempio di path relativo
+    frontend_ristorante::accoglienza::accogli_clientela();
+}
+
+// In Rust tutti gli elementi sono private nei confronti dei loro genitori. L'elemento child,
+// invece può sempre accedere al suo elemento parent.
+// Attenzione però! RENDERE UN MODULO PUBBLICO NON NE RENDE PUBBLICI I SUOI ELEMENTI!

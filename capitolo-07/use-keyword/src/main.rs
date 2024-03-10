e54f5f111fa6f0be7a5c4ce1c20d57931fca8340
Andrea <andrea.coi@outlook.it>
2024-03-10 21:27:39 +0100

    chiusura paragrafo 7.4

--- capitolo-07/use-keyword/src/main.rs
+++ capitolo-07/use-keyword/src/main.rs
@@ -1,3 +1,11 @@
+//tipi di inclusione file
+//glob
+use std::collections::*;
+// nested con self
+use std::io::{self, Write};
+// il paragrafo sugli import con use non è difficoltoso e sono tutte nozioni semplici o già
+// utilizzate in altri linguaggi.
+
 fn main() {
     println!("Hello, world!");
 }

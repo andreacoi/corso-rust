2efcbc23d4c4a75bd4957004b6ea56853be862ac
Andrea <andrea.coi@outlook.it>
2024-03-14 22:54:23 +0100

    modi di iterare i caratteri di una stringa
    REGOLA: Non utilizzare mai gli indici per iterare una stringa. Esistono metodi dedicati come chars o bytes.

--- capitolo-08/strings/src/main.rs
+++ capitolo-08/strings/src/main.rs
@@ -18,4 +18,27 @@ fn main() {
     // push_str, ad esempio è una funzione che consente di appendere dei parametri alla stringa,
     // senza "prenderne possesso", perché utilizzato passando la stringa sorgente e quella da
     // aggiungere come parametri della funzione stessa.
+    //
+    // # Accedere a caratteri singoli di una stringa utilizzando gli indici
+    // In RUST, È IMPOSSIBILE ACCEDERE A UNA STRINGA O A PARTE DI ESSA UTILIZZANDO GLI indici
+    let s2 = String::from("Ciao");
+    // s2[1] infatti restituirà un errore. Questo essenzialmente succede per due motivi:
+    // 1. Le modalità di storage delle stringhe
+    // 2. Il fatto che essenzialmente l'elemento "stringa", in Rust rappresenti un Wrapper attorno
+    //    a un Vec<u8>.
+    // REGOLA GENERALE
+    // MAI UTILIZZARE GLI INDICI PER SPEZZETTARE UNA STRINGA ED ACCEDERE AI RELATIVI VALORI.
+    // RUST stesso fornisce dei suggerimenti (DEGLI AVVERTIMENTI!) su come NON COMPORTARSI nel caso
+    // in cui si voglia effettuare lo slice di una stringa con gli indici.
+    // Esistono tuttavia dei metodi per eseguire un iterazione su una Stringa.
+    // Uno può essere:
+    //
+    for c in s2.chars() {
+        println!("{}", c);
+    }
+    // questo per visualizzare carattere per carattere la stringa s2.
+    // Se invece volessimo visualizzare i bytes, esiste un metodo anche per quello:
+    for b in s2.bytes() {
+        println!("{}", b);
+    }
 }

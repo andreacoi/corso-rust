82c09b52299067da396f424df2ceeefe02ebb70c
Andrea <andrea.coi@outlook.it>
2024-03-22 14:11:58 +0100

    riscrittura della gestione match con le closure + approfondimento unwrap ed expect

--- capitolo-09/recoverable-errors/src/main.rs
+++ capitolo-09/recoverable-errors/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     // può essere riscritto in questa maniera:
     // va integrata la libreria io ErrorKind che consente di gestire le casistiche d'errore.
     // p. 9.2.1 Match different errors
-    let gfile = match greetings_file_result {
+    /*     let gfile = match greetings_file_result {
         // 1
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -47,5 +47,31 @@ fn main() {
                 panic!("Problema nell'apertura del file: {:?}", other_error)
             }
         }, //2
-    }; // 1
+    }; // 1 */
+    // 9.2.1.1 Alternative all'utilizzo di match con Result<T, E>
+    // È possibile riscrivere il codice sopra utilizzando le closure per ottenere un codice più
+    // pulito e del tutto equivalente.
+
+    let gfile = File::open("file.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("file.txt").unwrap_or_else(|error| {
+                panic!("Errore nella creazione del file: {:?}", error);
+            })
+        } else {
+            panic!("Problema generico nell'apertura del file: {:?}", error);
+        }
+    });
+    // Le closure semplificano di molto la sintassi necessaria e vengono trattate nella maniera più
+    // specifica nel capitolo 13.
+    //
+    // 9.2.1.2 Scorciatoia per Panic sull'error: expect e unwrap.
+    // Oltre alle closure e a match sulle enum abbiamo un altro modo ancora più compatto per
+    // gestire i panic.
+    // Il modo è: utilizzare expect e unwrap. Expect consente di specificare un messaggio
+    // personalizzato nel caso di errore.
+    // Expect infatti ritorna T in caso di successo ed E in caso di errore.
+    // Unwrap è identico, a parte il fatto che non è possibile specificare un messaggio per il
+    // panic.
+    let new_file = File::open("gino.txt").expect("Il file non esiste.");
+    // Di solito gli sviluppatori Rust tendono ad utilizzare expect.
 }

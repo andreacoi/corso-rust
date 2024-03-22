7c966bd526671ebc5a6f875b6d6f0f813f9c89e1
Andrea <andrea.coi@outlook.it>
2024-03-22 13:01:26 +0100

    gestisco errori di vario tipo sull'esistenza o sulla creazione di un file grazie a match multipli

--- capitolo-09/recoverable-errors/src/main.rs
+++ capitolo-09/recoverable-errors/src/main.rs
@@ -1,4 +1,8 @@
 use std::fs::File;
+// integro la libreria ErrorKind
+// p. 9.2.1
+use std::io::ErrorKind;
+
 // Per gestire gli errori recuperabili RUST utilizza la enum Result<T, E> dove T ed E sono
 // parametri di tipo generico. Solitamente è un modo per stabilire il comportamento del programma
 // nei due stati previsti:
@@ -12,10 +16,36 @@ enum Result<T, E> {
 fn main() {
     let greetings_file_result = File::open("file.txt");
 
-    let gfile = match greetings_file_result {
+    /*
+    * è possibile gestire gli errori in questa maniera:
+    * SEMPLICE, con soli due casi, oppure cercando di prevedere altre casistiche d'errore.
+    *
+    * Per cui questo codice:
+    *  let gfile = match greetings_file_result {
         Ok(file) => file,
         Err(error) => {
             panic!("Il file non esiste: {:?}", error)
         }
-    };
+    }; */
+    // può essere riscritto in questa maniera:
+    // va integrata la libreria io ErrorKind che consente di gestire le casistiche d'errore.
+    // p. 9.2.1 Match different errors
+    let gfile = match greetings_file_result {
+        // 1
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // 2
+            ErrorKind::NotFound => {
+                // 3
+                match File::create("file.txt") {
+                    // 4
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Errore nella creazione del file: {:?}", e),
+                } // 4
+            } // 3
+            other_error => {
+                panic!("Problema nell'apertura del file: {:?}", other_error)
+            }
+        }, //2
+    }; // 1
 }

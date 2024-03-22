df9070789dc87f9bbfd4fd15d3bd8ea345145cbe
Andrea <andrea.coi@outlook.it>
2024-03-22 12:13:43 +0100

    apprende sintassi della enum result e prova a gestire il problema di un file non presente

--- capitolo-09/recoverable-errors/src/main.rs
+++ capitolo-09/recoverable-errors/src/main.rs
@@ -1,8 +1,14 @@
-// Per gestire gli errori recuperabili
+use std::fs::File;
+// Per gestire gli errori recuperabili RUST utilizza la enum Result<T, E> dove T ed E sono
+// parametri di tipo generico. Solitamente è un modo per stabilire il comportamento del programma
+// nei due stati previsti:
+// - se OK comportati in un certo modo,
+// - se ERR comportati in un altro.
+
 enum Result<T, E> {
     Ok(T),
     Err(E),
 }
 fn main() {
-    println!("Hello, world!");
+    let greetings_file = File::open("file.txt");
 }

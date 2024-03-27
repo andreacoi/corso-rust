a10fc52c7e177d1e905aaec40b8cbc53b45cd9ea
Andrea <andrea.coi@outlook.it>
2024-03-27 21:42:15 +0100

    inserisce intro sul file principale
    introduce guida recuperata qui --> https://carlomilanesi.github.io/linguaggio-rust

--- capitolo-10/generics/src/main.rs
+++ capitolo-10/generics/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+/*
+* Paragrafo 10.2 I generics
+* I generics vengono utilizzati per creare definizioni per oggetti come le firme di funzione o le
+* struct, che possiamo riutilizzare con molti e differenti tipi di dati concreti.
+* Talvolta, quando si scrive una funzione o un tipo di dati, potremmo volere che funzioni per più tipi di argomenti.
+* In Rust, lo possiamo fare usando i generici.
+* */
+fn main() {}

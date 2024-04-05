33538ec836707e4912230440e17ab44b6188e373
Andrea <andrea.coi@outlook.it>
2024-04-05 15:57:40 +0200

    scrive prime indicazioni sui lifetime

--- capitolo-10/lifetimes/src/main.rs
+++ capitolo-10/lifetimes/src/main.rs
@@ -1,3 +1,17 @@
+/* Lifetime - tempo di vita degli oggetti in Rust.
+* In Rust, ogni reference ha un Lifetime. Il Lifetime è lo scope per il quale quella determinata
+* reference è valida. Così come per i tipi anche le Lifetime possono essere implicite o subire
+* inferenza.
+* */
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let r = &x;
+    println!("r: {r}");
 }
+
+// 10.4.5 Annotazioni dei lifetime nelle funzioni
+// Le Annotazioni non hanno un grande significato di per sé. Servono solo ad indicare che la
+// reference ritornata dalla funzione è valida finché ENTRAMBI i parametri della funzione stessa
+// sono valide.
+// L'annotazione si scrive tra virgolette angolari dopo la dichiarazione della funzione, come i
+// generics utilizzando l'apostrofo e la definizione più breve possibile (solitamente a, b, c...).

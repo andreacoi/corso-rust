42b3f4ecf649fb934f474545bfd9bbd7142d075e
Andrea <andrea.coi@outlook.it>
2024-04-12 18:31:39 +0200

    scrive test fallimentare con macro panic!

--- capitolo-11/write-tests/src/lib.rs
+++ capitolo-11/write-tests/src/lib.rs
@@ -4,11 +4,17 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use core::panic;
+
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn altro_test() {
+        panic!("questo test fallirà inesorabilmente");
     }
 }

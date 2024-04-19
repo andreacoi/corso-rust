b7e064e6f121dac3e81159e6e3fc2c1668ca5020
Andrea <andrea.coi@outlook.it>
2024-04-19 18:41:29 +0200

    scrive test per capire se nella struct Rectangle il metodo can_hold funziona

--- capitolo-11/write-tests/src/lib.rs
+++ capitolo-11/write-tests/src/lib.rs
@@ -8,13 +8,28 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
     }
+
+    impl Rectangle {
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+
     #[test]
-    fn altro_test() {
-        panic!("questo test fallirà inesorabilmente");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 1,
+            height: 5,
+        };
+        assert!(larger.can_hold(&smaller));
     }
 }

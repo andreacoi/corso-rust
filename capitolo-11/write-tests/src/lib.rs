b7bb12a67c3f42c48308803e2c673ed31e072b73
Andrea <andrea.coi@outlook.it>
2024-04-20 12:12:49 +0200

    sposta la struct e le funzioni associate nella root del file
    rimuove anche una funzione non utilizzata (add)

--- capitolo-11/write-tests/src/lib.rs
+++ capitolo-11/write-tests/src/lib.rs
@@ -1,30 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use core::panic;
-
+    // questa dichiarazione use serve per accedere alle porzioni di codice fuori dal modulo test,
+    // che sono scritte direttamente nella root del file lib.rs
     use super::*;
 
-    #[derive(Debug)]
-    struct Rectangle {
-        width: u32,
-        height: u32,
-    }
-
-    impl Rectangle {
-        fn can_hold(&self, other: &Rectangle) -> bool {
-            self.width > other.width && self.height > other.height
-        }
-    }
-
+    /* Scrivo un test per testare il funzionamento della funzione can can_hold.
+     * La funzione larger_can_hold_smaller restituisce un assert, cioè il risultato di una verifica.
+     * In particolare, restituisce ok se il test procede per come stabilito e failure se invece
+     * assert non passa il test. Provando a cambiare i valori nelle due istanze della struct larger e smaller, il test restituisce FAILED.
+     * Ovviamente la command line risponderà con l'elenco dei failures, cioè dei test che sono falliti.
+     * */
     #[test]
     fn larger_can_hold_smaller() {
         let larger = Rectangle {
-            width: 8,
-            height: 7,
+            width: 11,
+            height: 14,
         };
         let smaller = Rectangle {
             width: 1,

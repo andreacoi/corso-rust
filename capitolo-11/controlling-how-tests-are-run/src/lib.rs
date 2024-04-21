7a65c085e3e9f2b8e3b85be88fc0e2480a21e3e0
Andrea <andrea.coi@outlook.it>
2024-04-21 17:01:40 +0200

    modalità di test, modalità per ignorare i test, avvio test multipli

--- capitolo-11/controlling-how-tests-are-run/src/lib.rs
+++ capitolo-11/controlling-how-tests-are-run/src/lib.rs
@@ -11,4 +11,20 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+    #[test]
+    #[ignore]
+    fn test_destinato_a_fallire() {
+        let value = 5;
+        assert_eq!(4, value);
+    }
+
+    /*
+     * Eseguire test multipli
+     * È possibile eseguire test multipli specificando una wildcard nel nome del test da eseguire.
+     * Se, per esempio ho due test chiamati add_three e add_two e un terzo test chiamato sub_two,
+     * posso far partire i test per add two e add_three digitando nel terminale cargo test add,
+     * perché i nomi delle funzioni di cui eseguire il test iniziano per add.
+     *
+     * Per ignorare i test, è sufficiente specificare #[ignore] dopo il decorator #[test].
+     * */
 }

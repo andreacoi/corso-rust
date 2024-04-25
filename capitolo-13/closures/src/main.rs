34cef1e6d076aec54d830369e6cd06a91d0d7066
Andrea <andrea.coi@outlook.it>
2024-04-25 14:16:17 +0200

    scrive funzioni base per il funzionamento di Inventory - da passare al main
    todo: scrivere funzioni nel main che si integrano con quelle già realizzate.

--- capitolo-13/closures/src/main.rs
+++ capitolo-13/closures/src/main.rs
@@ -1,6 +1,6 @@
 // 13.1 Le closure
 // Le closure sono delle funzioni anonime che possono "salvate" in una variabile o passate come
-// argomenti di una funzione. La closure può essere creata in un punto del codice e poi può essere
+// argomenti di una funzione. La closure può essere creata in un punto del codice e poi può esser
 // valutata dovunque, in differenti contesti (context).
 // Diveramente dalle funzioni però, le closure possono "catturare" valori a partire dallo scope nel
 // quale sono definite.
@@ -23,4 +23,49 @@
 // Un altro componente importante è una funzione che ci faccia sapere la maglietta con la maggiore
 // giacenza in magazzino.
 // All'interno del main, faremo i controlli per l'estrazione della maglietta.
+//
+// Funzioni associate
+// La funzione giveaway e quella most_stocked sono funzioni associate di Inventory.
+#[derive(Debug, PartialEq, Copy, Clone)]
+
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+// Dichiaro le funzioni associate a Inventory.
+impl Inventory {
+    // argomenti di giveaway:
+    // &self -> Inventory
+    // user_preference: UNA delle Opzioni di ShirtColor -> Option<ShirtColor>
+    //
+    // Ritorna:
+    // ShirtColor
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        // ritorno user_preference con unwrap_or_else
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+    // argomenti di most_stocked:
+    // &self --> Inventory
+    // Ritorna:
+    // enum di tipo ShirtColor
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_blue > num_red {
+            ShirtColor::Blue
+        } else {
+            ShirtColor::Red
+        }
+    }
+}
 fn main() {}

213f1011d8193ab0a41b2592a1d034686ce40ab4
Andrea <andrea.coi@outlook.it>
2024-05-01 19:45:48 +0200

    riproduce funzione funzione nel Listing 13.16

--- capitolo-13/iterators/src/lib.rs
+++ capitolo-13/iterators/src/lib.rs
@@ -7,3 +7,17 @@ fn iterator_sum() {
     let total: u32 = v1_iter.sum();
     assert_eq!(total, 6);
 }
+
+#[derive(PartialEq, Debug)]
+
+struct Scarpa {
+    tipo: String,
+    misura: u32,
+}
+
+fn scarpe_per_ogni_misura(scarpa: Vec<Scarpa>, misura_scarpa: u32) -> Vec<Scarpa> {
+    scarpa
+        .into_iter()
+        .filter(|m| m.misura == misura_scarpa)
+        .collect()
+}

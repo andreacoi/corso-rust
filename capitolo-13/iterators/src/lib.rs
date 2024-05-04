6d062020db1d30a29c2e8149efcf48583d998ad1
Andrea <andrea.coi@outlook.it>
2024-05-04 12:21:47 +0200

    stesura test per provare closure in combinazione con gli iteratori
    chiusura paragrafo

--- capitolo-13/iterators/src/lib.rs
+++ capitolo-13/iterators/src/lib.rs
@@ -17,12 +17,45 @@ mod tests {
 
     #[test]
     fn filtra_scarpe_per_misura() {
+        // avendo creato un vettore di struct, ho creato, quindi, un insieme da cui attingere per
+        // fare le mie ricerche sui numeri disponibili. Quindi ho:
+        // - delle ballerine numero 40;
+        // - delle Sneakers numero 42;
+        // - delle scarpe anti-infortunistica numero 42;
+        // - degli stivali numero 39.
+        // A questo punto devo verificare che la funzione che mi restituisce le scarpe per ogni
+        // misura funzioni.
+        // In breve devo verificare che indicando, ad esempio, il numero 39 la funzione
+        // scarpe_per_ogni_misura mi restituisca un vettore con all'interno solo il paio di
+        // stivali. Infatti, come 39 ho solo degli stivali.
         let scarpe = vec![
             Scarpa {
                 tipo: String::from("Ballerine"),
                 misura: 40,
-            }, // TODO: INSERIRE ALTRI MODELLI DI SCARPE CON ULTERIORI MISURE E PROVARE LA FUNZIONE.
+            },
+            Scarpa {
+                tipo: String::from("Sneakers"),
+                misura: 42,
+            },
+            Scarpa {
+                tipo: String::from("Anti-Infortunistica"),
+                misura: 42,
+            },
+            Scarpa {
+                tipo: String::from("Stivali"),
+                misura: 39,
+            },
         ];
+        //
+        let scarpe_della_mia_misura = scarpe_per_ogni_misura(scarpe, 40);
+        // eseguo la verifica del test
+        assert_eq!(
+            scarpe_della_mia_misura,
+            vec![Scarpa {
+                tipo: String::from("Ballerine"),
+                misura: 40,
+            }]
+        )
     }
 
     #[test]

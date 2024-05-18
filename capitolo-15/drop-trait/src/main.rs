f908c2308392ba1674dd731eecc72fd44d9404b0
Andrea <andrea.coi@outlook.it>
2024-05-18 12:47:26 +0200

    scrive introduzione sul trait Drop negli smart pointer

--- capitolo-15/drop-trait/src/main.rs
+++ capitolo-15/drop-trait/src/main.rs
@@ -1,3 +1,21 @@
+/*
+* 15.3 Esempi di utilizzo del Drop Trait
+* Il trait drop viene utilizzato, a differenza dei linguaggi che utilizzano i garbage collector per
+* svuotare quella porzione di memoria ogni qualvolta una variabile esce fuori dallo scope.
+* Questo comportamento prende il nome di drop (buttare).
+* Rust infatti, non appena una variabile esce fuori dallo scope, invoca (dietro le quinte) un
+* distruttore che si occupa di smaltire la porzione di heap associata a quella variabile,
+* distruggendo di fatto la variabile stessa.
+* Questo trait, benché sia caratteristico e idiomatico di Rust, nonché una delle sue funzionalità
+* più note del linguaggio, viene trattato nel capitolo degli smart pointer perché la sua
+* implementazione viene utilizzata PROPRIO nel contesto degli smart pointer.
+* Ho parlato di implementazione perché tramite impl è possibile scrivere una implementazione
+* dedicata del trait drop specifica per ogni smart pointer o per ogni struct di tipo Box<T>.
+* Le implementazioni di drop possono variare, ma il fine ultimo rimane sempre il medesimo.
+* Quello che può essere influenzato è il comportamento dell'istanza QUANDO STA PER uscire dallo
+* scope.
+* Scrivo un esempio dichiarando uno smart pointer custom.
+* */
 fn main() {
     println!("Hello, world!");
 }

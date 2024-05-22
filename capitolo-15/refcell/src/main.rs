5c3e9bdfb2ea6cfd381d65e53a3320e77a6ddfc2
Andrea <andrea.coi@outlook.it>
2024-05-22 23:09:38 +0200

    appunto su paragrafo da trattare

--- capitolo-15/refcell/src/main.rs
+++ capitolo-15/refcell/src/main.rs
@@ -3,6 +3,8 @@
 // La differenza con Box<T> è che i dati contenuti in Box<T> non sono mutabili, a differenza dei
 // dati contenuti in RefCell. Box<T> inoltre verifica la mutabilità a compile time, RefCell, invece
 // lo fa a runtime.
+// TODO: PROVARE A SCRIVERE LIBRERIA PER CHIAMATE A SERVIZIO API PER LA GESTIONE DEI MESSAGGI PAG.
+// 337.
 fn main() {
     println!("Hello, world!");
 }
